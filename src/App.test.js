3ad27de4c6296bbba3baa56767049a098d776f20
Naresh-Kanakam <36816342+Naresh-Kanakam@users.noreply.github.com>
2020-06-30 03:17:58 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders component', () => {
+  const RenderedFile = render(<App />);
+  expect(RenderedFile).not.toBe(null);
+});
+
+test('renders edt text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = getByText('Erat Libero');
+  expect(getByText('Erat Libero')).toBeInTheDocument()
 });
